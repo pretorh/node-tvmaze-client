d4b8ba7c3134d0bc800251469daa20aef8906b73
Hendri Pretorius <pretorh@gmail.com>
2015-09-20 19:57:26 +0200

    search via request

--- test/search.js
+++ test/search.js
@@ -1,6 +1,22 @@
 var assert = require('assert');
 var nock = require('nock');
+var fs = require('fs');
 var tvmaze = require('../');
 
 describe('search', function() {
+    var search = nock(tvmaze.options.rootUrl)
+        .persist()
+        .get('/search/shows?q=robot')
+        .reply(200, fs.readFileSync(__dirname + '/testdata/shows.json'));
+    after(function() {
+        nock.cleanAll();
+        nock.restore();
+    });
+
+    it('search via the url', function(done) {
+        tvmaze.search('robot', function(err) {
+            search.done();
+            done();
+        });
+    })
 });
