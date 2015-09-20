91d34c8515639011f886222ec5b697fb86b555cc
Hendri Pretorius <pretorh@gmail.com>
2015-09-20 21:35:22 +0200

    search for show based on the first cli argument

--- bin/tvmaze-client.js
+++ bin/tvmaze-client.js
@@ -1,1 +1,12 @@
 var tvmaze = require('../');
+
+tvmaze.search(process.argv[2], printReponse);
+
+function printReponse(err, res) {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    console.log(JSON.stringify(res, null, 2));
+}
