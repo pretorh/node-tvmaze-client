0c47c794d929a2184b412cd4b1fa553a137d8d80
Hendri Pretorius <pretorh@gmail.com>
2015-09-20 20:33:15 +0200

    setup gulpfile
    gulp test runs mocha tests

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('test', function() {
+    return gulp.src(['test/*.js'], { read: false })
+        .pipe(mocha())
+        .on('error', console.error);
+});
